38c0a48e3c9ff603c1ef9e3eee7693a83759544f
AlexGoIT <alexandrpuryaev@gmail.com>
2023-07-22 18:18:18 +0300

    add material ui

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,17 +1,43 @@
 import PropTypes from 'prop-types';
+import Button from '@mui/material/Button';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 import { OptionsWrapper } from './FeedbackOptions.styled';
+
+const theme = createTheme({
+  palette: {
+    good: {
+      main: '#1ca700',
+    },
+    neutral: {
+      main: '#ffbb00',
+    },
+    bad: {
+      main: '#ff3939',
+    },
+  },
+});
 
 const FeedbackOptions = ({ onClick, feedbackOptions }) => {
   return (
-    <OptionsWrapper>
-      {Object.keys(feedbackOptions).map((key, ind) => {
-        return (
-          <button key={ind} onClick={() => onClick(key)}>
-            {key}
-          </button>
-        );
-      })}
-    </OptionsWrapper>
+    <ThemeProvider theme={theme}>
+      <OptionsWrapper>
+        {Object.keys(feedbackOptions).map((key, ind) => {
+          return (
+            <Button
+              className="optionButton"
+              key={ind}
+              onClick={() => onClick(key)}
+              color={key}
+              size="large"
+              variant="contained"
+            >
+              {key}
+            </Button>
+          );
+        })}
+      </OptionsWrapper>
+    </ThemeProvider>
   );
 };
 
--- src/components/FeedbackOptions/FeedbackOptions.styled.js
+++ src/components/FeedbackOptions/FeedbackOptions.styled.js
@@ -4,32 +4,19 @@ export const OptionsWrapper = styled.div`
   display: flex;
   gap: 10px;
 
-  button {
-    width: 80px;
+  .optionButton {
+    min-width: 120px;
     height: 40px;
     border-radius: 5px;
     border: none;
     font-size: 18px;
     font-weight: bold;
-    background-color: #f1f1f1;
-    opacity: 0.75;
     cursor: pointer;
-    transition: opacity 0.2s ease-in;
-  }
+    opacity: 0.8;
+    transition: box-shadow 0.2s ease-in, opacity 0.2s ease-in;
 
-  button:nth-child(1) {
-    background-color: green;
-  }
-
-  button:nth-child(2) {
-    background-color: orange;
-  }
-
-  button:nth-child(3) {
-    background-color: red;
-  }
-
-  button:hover {
-    opacity: 1;
+    &:is(:hover, :focus) {
+      opacity: 1;
+    }
   }
 `;
